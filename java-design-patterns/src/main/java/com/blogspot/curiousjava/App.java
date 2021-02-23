1d2749cfeccc47717a0e35df3912f99590107be4
Utkarsh Thakkar <utkarsh.javadev@gmail.com>
2021-02-23 17:40:49 -0600

    Initial Commit

---
+++ java-design-patterns/src/main/java/com/blogspot/curiousjava/App.java
@@ -0,0 +1,13 @@
+package com.blogspot.curiousjava;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
